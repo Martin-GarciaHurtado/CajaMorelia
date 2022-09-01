8e47460b6075a6534fe05c10b34f3fc59c948e58
Martin Garcia Hurtado <martingarciahurtado@gmail.com>
2022-08-31 20:50:22 -0500

    Agregado de v-dialog para el crud y visualizar la informacion

--- examen/src/components/HelloWorld.vue
+++ examen/src/components/HelloWorld.vue
@@ -1,37 +1,67 @@
 <template>
   <v-container>
+    <div class="my-3">
+      <v-btn color="primary" rounded @click="agregar=true">
+        Agregar cliente<v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+      <br>
+    </div>
     <template>
       <v-simple-table dark>
-        <template v-slot:default>
+        <template v-slot:default v-if="clientes">
           <thead>
             <tr>
+              <th class="text-left">
+                id
+              </th>
               <th class="text-left">
                 Name
               </th>
               <th class="text-left">
-                Cuenta
-              </th>
-              <th class="text-left">
-                Tipo de cuenta
+                rfc
               </th>
               <th>
                 Informacion
+              </th>
+              <th>
+                Acciones
               </th>
             </tr>
           </thead>
           <tbody>
             <tr
-              v-for="item in desserts"
-              :key="item.name"
+              v-for="(item,i) in clientes.data"
+              :key="i"
             >
-              <td>{{ item.name }}</td>
-              <td>{{ item.calories }}</td>
-              <td></td>
-              <td><v-icon @click="Info()">mdi-information</v-icon></td>
+              <td>{{ item.id }}</td>
+              <td>{{ item.attributes.nombre }}</td>
+              <td>{{ item.attributes.rfc}}</td>
+              <td><v-icon @click="Info(item.id)">mdi-information</v-icon></td>
+              <td><v-icon @click="Update(item.id)">mdi-pencil</v-icon><v-icon @click="Delete(item.id)">mdi-delete</v-icon></td>
             </tr>
           </tbody>
         </template>
       </v-simple-table>
+    </template>
+    <template>
+      <v-dialog v-model="info" width="1000">
+
+      </v-dialog>
+    </template>
+    <template>
+      <v-dialog v-model="agregar" width="1000">
+
+      </v-dialog>
+    </template>
+    <template>
+      <v-dialog v-model="editar" width="1000">
+
+      </v-dialog>
+    </template>
+    <template>
+      <v-dialog v-model="eliminar" width="1000">
+
+      </v-dialog>
     </template>
   </v-container>
 </template>
@@ -42,62 +72,55 @@
   export default {
     data () {
       return {
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
+        clientes: null,
+        cuenta: null,
+        tipoCuenta: null,
+        agregar: false,
+        editar: false,
+        eliminar: false,
+        info: false
       }
     },
     methods: {
-      Info(){
-        console.log("Funciona")
+      Info(id){
+        this.info = true
+        console.log(id)
       },
       get_Clientes(){
         axios.get( 'http://localhost:1337/api/clientes' ).then( res => {
           console.log(res.data)
+          this.clientes = res.data
         })
         .catch( e => {
           console.log(e)
         })
-      }
+      },
+      get_Cuenta(){
+        axios.get( 'http://localhost:1337/api/cliente-cuentas/:id' ).then( res => {
+          console.log(res.data)
+          this.cuenta = res.data
+        })
+        .catch( e => {
+          console.log(e)
+        })
+      },
+      get_TipoCuenta(){
+        axios.get( 'http://localhost:1337/api/tipo-cuentas/:id' ).then( res => {
+          console.log(res.data)
+          this.tipoCuenta = res.data
+        })
+        .catch( e => {
+          console.log(e)
+        })
+      },
+      Update(id){
+        this.editar = true
+        console.log(id)
+      },
+      Delete(id){
+        this.eliminar = true
+        console.log(id)
+      },
     },
     created() {
       this.get_Clientes();
